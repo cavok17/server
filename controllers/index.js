328385013d010b5328e0e0905f535fd597a35b3d
sangil <sangil.yun@opensky.co.kr>
2020-11-19 14:43:53 +0900

    ing

--- controllers/index.js
+++ controllers/index.js
@@ -62,12 +62,48 @@ const change_index_level = async(req, res) => {
     console.log('인덱스 레벨을 변경합니다.');
     console.log(req.body);
 
-    const name_modi_result = await Index.updateOne(
-        {_id : req.body.index_id},
-        {level : req.body.level}
-    );
-
-    get_indexList(req, res); 
+    // 앞에 시퀀스의 인덱스를 확인하고
+    // 앞 시퀀스 레벨 -1보다는 더 내려가면 안 됨
+    let upper_index = await Index
+        .find({book_id : req.body.book_id,
+                seq : {$lt : req.body.seq}})                
+        .sort({seq : -1})
+        .limit(1)
+    
+    // 다음으로 나오는 동일(또는 더 높은) 레벨 인덱스 위치를 확인하고
+    let next_same_level_index = await Index
+        .find({book_id : req.body.book_id,
+                seq : {$gt : req.body.seq},
+                level : {$lte : req.body.level}})
+        .sort({seq : 1})
+        .limit(1)
+    let next_same_level_index_seq;
+    if (next_same_level_index.index ===0){
+        next_same_level_index_seq = req.body.seq
+    } else {
+        next_same_level_index_seq = next_same_level_index[0].seq
+    };
+    
+    // 이제 이동을 시켜보자
+    // 현재 이동하려는 레벨이 upper레벨보다 오른쪽에 있으면 오른쪽 이동은 안 돼
+    if(req.body.action == 'right'){
+        if (req.body.level > upper_index[0].seq) {
+            console.log("이동 불가")
+            res.json({isloggedIn : true, msg : '이동불가' });
+        } else {
+            let level_modi_result = await Index.updateMany(
+                {book_id : req.body.book_id,
+                seq : {$gte : req.body.seq, $lte : next_same_level_index_seq}},
+                {$inc : {level : 1}});
+            get_indexList(req, res); 
+        }
+    } else if(req.body.action == 'left'){
+        let level_modi_result = await Index.updateMany(
+            {book_id : req.body.book_id,
+            seq : {$gte : req.body.seq, $lte : next_same_level_index_seq}},
+            {$inc : {level : -1}});
+        get_indexList(req, res); 
+    };    
 };
 
 // 인덱스의 순서를 변경합니다.
@@ -118,6 +154,7 @@ const delete_index = async(req, res) => {
                 seq : {$gt : req.body.seq},
                 level : {$lte : req.body.level}})
         .sort({seq : 1})
+        .limit(1)
     
     // 없으면 아래 녀석들 다 레벨을 하나씩 올려주고
     if (next_same_level_index.length === 0){
