a63215d1cd508d3289039b43992b2bc650eab701
sangil <sangil.yun@opensky.co.kr>
2020-11-23 17:20:00 +0900

    ing

--- controllers/card.js
+++ controllers/card.js
@@ -56,6 +56,65 @@ exports.create_card = async (req, res) => {
 
 };
 
+// 카드 순서를 변경합니다.
+exports.create_card = async (req, res) => {
+    console.log("카드 순서를 변경합니다.");
+    console.log(req.body);
+
+    let current_seq_card = await Card.findOne({_id : req.body.card_id});
+    let current_seq = current_seq_card[0].seq_in_index;
+    let target_seq = req.body.target_seq;
+
+    if (current_seq > target_seq){
+        let seq_change_result = await Card.updateMany(
+            {index_id : req.body.index_id, 
+            seq_in_index : {$gt : target_seq},
+            seq_in_index : {$lt : current_seq}},
+            {$inc : {seq_in_index : 1}})
+        current_seq_card.seq_in_index = target_seq +1;
+        current_seq_card = await current_seq_card.save();
+    } else {
+        let seq_change_result = await Card.updateMany(
+            {index_id : req.body.index_id, 
+            seq_in_index : {$gt : current_seq},
+            seq_in_index : {$lte : target_seq}},
+            {$inc : {seq_in_index : 1}})
+        current_seq_card.seq_in_index = target_seq +1;
+        current_seq_card = await current_seq_card.save();
+    } 
+
+    let cardlist = get_cardlist_func(req.body.index_id)
+
+    res.json({isloggedIn : true, cardlist});
+};
+
+// 카드 내용을 변경합니다.
+exports.create_card = async (req, res) => {
+    console.log("카드 내용을 변경합니다.");
+    console.log(req.body);
+
+    let card_edit_result = await Card.updateOne(
+        {_id : req.body.card_id},
+        {
+           cardtype_id : req.body.cardtype_id,
+           importance :  req.body.importance,
+        });
+    let content_edit_result = await Content.updateOne(
+        {card_id : req.body.card_id},
+        {
+           first_face : req.body.first_face,
+           second_face : req.body.second_face,
+           third_face : req.body.third_face,
+           annotation : req.body.annotation,           
+        }
+    );    
+
+    let cardlist = get_cardlist_func(req.body.index_id)
+
+    res.json({isloggedIn : true, cardlist});
+};
+
+
 // max 시퀀스를 찾아드립니다.
 const get_max_seq = async (index_id) => {   
     let max_seq_card = await Card
