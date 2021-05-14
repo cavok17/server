c7ce7d8dc87ca78738bf05c45031b3777a8afe5f
sangil <sangil.yun@opensky.co.kr>
2021-05-14 11:51:14 +0900

    ing

--- routes/user.js
+++ routes/user.js
@@ -61,7 +61,7 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
         console.error(loginError);
         return next(loginError);
       }      
-      return res.json({'isLoggedIn' : true});      
+      return res.json({'isLoggedIn' : true, user});      
     });
   })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
 });
